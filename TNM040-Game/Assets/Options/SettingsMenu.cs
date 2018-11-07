437072312c3d2612adcb8ee58b45b6eeeeebd308
Davidsundberg97 <davidsundberg97@gmail.com>
2018-11-07 12:52:53 +0100

    Fullscreen

---
+++ TNM040-Game/Assets/Options/SettingsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+    }
+}
