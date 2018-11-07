b51002ea12e9c91f606f56f24dac357ac1aba25d
Davidsundberg97 <davidsundberg97@gmail.com>
2018-11-07 16:30:38 +0100

    timer

--- TNM040-Game/Assets/gameMaster.cs
+++ TNM040-Game/Assets/gameMaster.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameMaster : MonoBehaviour {
 
-    public int points;
+    public int points = 0;
+    Text pointScore;
+
+    private void Start()
+    {
+        pointScore = GetComponent<Text> ();
+    }
 
     private void Update()
     {
         //pointsText = ("Points: " + points);
+        pointScore.text = "Score: " + points;
         Debug.Log(points);
     }
 }
