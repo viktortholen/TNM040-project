e5356ef33294fff131e2fbfd22c6ee66e5cd2b1a
Philip Ngo <philipngo_@hotmail.com>
2018-11-09 14:13:30 +0100

    Projectiletrap

---
+++ TNM040-Game/Assets/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Projectile : MonoBehaviour {
+
+    public Transform projectilePoint;
+    public GameObject bulletPrefab;
+
+    float timeLeft = 2.0f;
+
+    // Update is called once per frame
+    void Update () {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            Shoot();
+            timeLeft = 2.0f;
+        }
+    }
+
+    void Shoot()
+    {
+        //Shooting logic
+        Instantiate(bulletPrefab, projectilePoint.position, projectilePoint.rotation);
+
+    }
+}
