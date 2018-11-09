a2be0e0fac3cd432abbc086b24772540321d059f
viktortholen <viktor.tholen@gmail.com>
2018-11-09 15:01:11 +0100

    Coins moved to separate script

---
+++ TNM040-Game/Assets/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    public gameMaster gm;
+    // Use this for initialization
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<gameMaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+            gm.points++;
+        }
+    }
+}
--- TNM040-Game/Assets/Furball Assets/FurBall2D/Scripts/PlayerController.cs
+++ TNM040-Game/Assets/Furball Assets/FurBall2D/Scripts/PlayerController.cs
@@ -1,120 +1,125 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-	
-	public float maxSpeed = 6f;
-	public float jumpForce = 1000f;
-	public Transform groundCheck;
-	public LayerMask whatIsGround;
-	public float verticalSpeed = 20;
-	[HideInInspector]
-	public bool lookingRight = true;
-	bool doubleJump = false;
-	public GameObject Boost;
-	
-	private Animator cloudanim;
-	public GameObject Cloud;
+public class PlayerController : MonoBehaviour
+{
+
+    public float maxSpeed = 6f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public float verticalSpeed = 20;
+    [HideInInspector]
+    public bool lookingRight = true;
+    bool doubleJump = false;
+    public GameObject Boost;
+
+    private Animator cloudanim;
+    public GameObject Cloud;
     public bool canJump = false;
     public bool canDown = false;
     public bool canLeft = false;
     public bool canRight = true;
 
     private Rigidbody2D rb2d;
-	private Animator anim;
-	private bool isGrounded = false;
-    private gameMaster gm;
-
-	// Use this for initialization
-	void Start () {
-		rb2d = GetComponent<Rigidbody2D>();
-		anim = GetComponent<Animator>();
-        //cloudanim = GetComponent<Animator>();
-        gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<gameMaster>();
-		Cloud = GameObject.Find("Cloud");
-  		//cloudanim = GameObject.Find("Cloud(Clone)").GetComponent<Animator>();
-	}
+    private Animator anim;
+    private bool isGrounded = false;
 
 
-	void OnCollisionEnter2D(Collision2D collision2D) {
-		
-		if (collision2D.relativeVelocity.magnitude > 20){
-			Boost = Instantiate(Resources.Load("Prefabs/Cloud"), transform.position, transform.rotation) as GameObject;
-		//	cloudanim.Play("cloud");	
-
-		}
-	}
-    private void OnTriggerEnter2D(Collider2D collision)
+    // Use this for initialization
+    void Start()
     {
-        if (collision.CompareTag("Coin"))
-        {
-            Destroy(collision.gameObject);
-            gm.points++;
-        }
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        //cloudanim = GetComponent<Animator>();
+
+        Cloud = GameObject.Find("Cloud");
+
+        //cloudanim = GameObject.Find("Cloud(Clone)").GetComponent<Animator>();
     }
 
 
+    void OnCollisionEnter2D(Collision2D collision2D)
+    {
+
+        if (collision2D.relativeVelocity.magnitude > 20)
+        {
+            Boost = Instantiate(Resources.Load("Prefabs/Cloud"), transform.position, transform.rotation) as GameObject;
+            //	cloudanim.Play("cloud");	
+
+        }
+    }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
 
-	if (Input.GetButtonDown("Jump") && (isGrounded || !doubleJump) && canJump)
-		{
-			rb2d.AddForce(new Vector2(0,jumpForce));
+        if (Input.GetButtonDown("Jump") && (isGrounded || !doubleJump) && canJump)
+        {
+            rb2d.AddForce(new Vector2(0, jumpForce));
 
-			if (!doubleJump && !isGrounded)
-			{
-				doubleJump = true;
-				Boost = Instantiate(Resources.Load("Prefabs/Cloud"), transform.position, transform.rotation) as GameObject;
-			//	cloudanim.Play("cloud");		
-			}
-		}
-
-
-	if (Input.GetButtonDown("Vertical") && !isGrounded && canDown)
-		{
-			rb2d.AddForce(new Vector2(0,-jumpForce));
-			Boost = Instantiate(Resources.Load("Prefabs/Cloud"), transform.position, transform.rotation) as GameObject;
-			//cloudanim.Play("cloud");
-		}
-
-	}
+            if (!doubleJump && !isGrounded)
+            {
+                doubleJump = true;
+                Boost = Instantiate(Resources.Load("Prefabs/Cloud"), transform.position, transform.rotation) as GameObject;
+                //	cloudanim.Play("cloud");		
+            }
+        }
 
 
-	void FixedUpdate()
-	{
-		if (isGrounded) 
-			doubleJump = false;
+        if (Input.GetButtonDown("Vertical") && !isGrounded && canDown)
+        {
+            rb2d.AddForce(new Vector2(0, -jumpForce));
+            Boost = Instantiate(Resources.Load("Prefabs/Cloud"), transform.position, transform.rotation) as GameObject;
+            //cloudanim.Play("cloud");
+        }
+
+    }
+
+
+    void FixedUpdate()
+    {
+        if (isGrounded)
+            doubleJump = false;
 
         float hor;
-  
+
         hor = Input.GetAxis("Horizontal");
 
 
 
-		anim.SetFloat ("Speed", Mathf.Abs (hor));
+        anim.SetFloat("Speed", Mathf.Abs(hor));
 
-            rb2d.velocity = new Vector2(hor * maxSpeed, rb2d.velocity.y);
-		
-		  
-		isGrounded = Physics2D.OverlapCircle (groundCheck.position, 0.15F, whatIsGround);
-
-		anim.SetBool ("IsGrounded", isGrounded);
-
-		if ((hor > 0 && !lookingRight)||(hor < 0 && lookingRight))
-			Flip ();
-		 
-		anim.SetFloat ("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
-	}
+        rb2d.velocity = new Vector2(hor * maxSpeed, rb2d.velocity.y);
 
 
-	
-	public void Flip()
-	{
-		lookingRight = !lookingRight;
-		Vector3 myScale = transform.localScale;
-		myScale.x *= -1;
-		transform.localScale = myScale;
-	}
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.15F, whatIsGround);
+
+        anim.SetBool("IsGrounded", isGrounded);
+
+        if ((hor > 0 && !lookingRight) || (hor < 0 && lookingRight))
+            Flip();
+
+        anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+
+
+    public void Flip()
+    {
+        lookingRight = !lookingRight;
+        Vector3 myScale = transform.localScale;
+        myScale.x *= -1;
+        transform.localScale = myScale;
+    }
 
 }
+
+
+
+
+
+
+
+
