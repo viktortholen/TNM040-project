d3f09a78e443bdec37f8faa93abfb42bb8c71b00
Davidsundberg97 <davidsundberg97@gmail.com>
2018-11-09 14:33:25 +0100

    EndScreen

--- TNM040-Game/Assets/MainMenu.cs
+++ TNM040-Game/Assets/MainMenu.cs
@@ -10,6 +10,16 @@ public class MainMenu : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void MainMeny()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(1);
+    }
+
     public void QuitGame()
     {
         Debug.Log("QUIT!");
