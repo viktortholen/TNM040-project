8b7aff0dbbe204e27d2aea4f41bb8a6d5c6777f7
viktortholen <viktor.tholen@gmail.com>
2018-11-10 01:17:34 +0100

    Particle Coin, fixed tileproblem

--- TNM040-Game/Assets/Coin.cs
+++ TNM040-Game/Assets/Coin.cs
@@ -5,22 +5,18 @@ public class Coin : MonoBehaviour
 {
 
     public gameMaster gm;
+    public GameObject CoinParticle;
     // Use this for initialization
     void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<gameMaster>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            Instantiate(CoinParticle, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
             gm.points++;
         }
