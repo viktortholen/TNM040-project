a17d7463eb422b7c7767861c303123024382eef1
Philip Ngo <philipngo_@hotmail.com>
2018-11-14 14:21:02 +0100

    Bullet change

--- TNM040-Game/Assets/Bullet.cs
+++ TNM040-Game/Assets/Bullet.cs
@@ -15,7 +15,11 @@ public class Bullet : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
         Debug.Log(hitInfo.name);
-        Destroy(gameObject);
+        if(hitInfo.CompareTag("Player") || hitInfo.compareTag("Props"))
+        {
+            Destroy(gameObject);
+        }
+        
     }
 
 }
