e1f786ddbb6037adf8d0212a8037768f505b827a
goshaaz <44804648+goshaaz@users.noreply.github.com>
2018-11-14 13:57:25 +0100

    Flera checkpoints

--- TNM040-Game/Assets/Levelmanager scripts/Checkpoint.cs
+++ TNM040-Game/Assets/Levelmanager scripts/Checkpoint.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public LevelManager levelManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update () {
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            levelManager.currentCheckpoint = gameObject;
+        }
+    }
 }
