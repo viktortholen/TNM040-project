4890f62401ec5c6ecbfd96bdfc5e294eff95e876
Davidsundberg97 <davidsundberg97@gmail.com>
2018-11-14 16:58:26 +0100

    UIEnabler

---
+++ TNM040-Game/Assets/UIEnabler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIEnabler : MonoBehaviour {
+
+    public TextMeshProUGUI Score;
+
+    private void Start()
+    {
+        Score.GetComponent<TextMeshProUGUI>();
+        Score.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Score.GetComponent<TextMeshProUGUI>();
+            Score.enabled = true;
+
+        }
+    }
+}
