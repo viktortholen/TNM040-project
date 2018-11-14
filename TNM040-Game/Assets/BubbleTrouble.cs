ca437a30d7bef884e1bfc8e5c949145432d4b47c
viktortholen <viktor.tholen@gmail.com>
2018-11-14 15:48:35 +0100

    WaterBubbles

---
+++ TNM040-Game/Assets/BubbleTrouble.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleTrouble : MonoBehaviour {
+
+    public GameObject Player;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player.GetComponent<Rigidbody2D>().gravityScale = -0.5f;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player.GetComponent<Rigidbody2D>().gravityScale = 1f;
+        }
+    }
+
+
+
+}
