433163615beb4e86dbd24e5a98647a07f4bb64dd
goshaaz <44804648+goshaaz@users.noreply.github.com>
2018-11-18 14:14:19 +0100

    Script for movescene

---
+++ TNM040-Game/Assets/MoveScene/MoveScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour
+{
+    [SerializeField] private string loadLevel;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(loadLevel);
+        }
+    }
+
+
+}
