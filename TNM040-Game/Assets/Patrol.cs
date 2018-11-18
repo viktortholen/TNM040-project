eac0e93d78c07f72c18a42261116df1685678d34
viktortholen <viktor.tholen@gmail.com>
2018-11-18 15:27:23 +0100

    AI

--- TNM040-Game/Assets/Patrol.cs
+++ TNM040-Game/Assets/Patrol.cs
@@ -8,23 +8,35 @@ public class Patrol : MonoBehaviour {
     private bool movingRight = true;
     public Transform GroundDetection;
 
-    void update ()
+    Vector2 direction;
+
+    private void Start()
+    {
+        direction = Vector2.right;
+    }
+
+
+    void Update ()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
-
+       
         RaycastHit2D groundInfo = Physics2D.Raycast(GroundDetection.position, Vector2.down, 2f);
+        RaycastHit2D wallinfo = Physics2D.Raycast(GroundDetection.position, direction, 0.1f);
 
-        if(groundInfo.collider == false)
+
+        if (groundInfo.collider == false || wallinfo.collider.CompareTag("Base") != false)
         {
             if(movingRight == true)
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
                 movingRight = false;
+                
             }
             else
             {
                 transform.eulerAngles = new Vector3(0, 0, 0);
                 movingRight = true;
+              
             }
 
         }
