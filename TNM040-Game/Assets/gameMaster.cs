8964acc779058d1a1599e1777c12f0ad40ec754d
Davidsundberg97 <davidsundberg97@gmail.com>
2018-11-21 14:26:44 +0100

    Score Dynamic Update

--- TNM040-Game/Assets/gameMaster.cs
+++ TNM040-Game/Assets/gameMaster.cs
@@ -9,16 +9,25 @@ public class gameMaster : MonoBehaviour {
     public int points;
     public TextMeshProUGUI PointScore;
     public TextMeshProUGUI YouWinMessage;
+    public GameObject Grid;
+
     ////private void Start()
     ////{
     ////    PointScore = GetComponent<Text> ();
     ////}
+   
+
+
+    
+   
 
     private void Update()
     {
+        int  childs = Grid.transform.childCount;
         //pointsText = ("Points: " + points);
-        PointScore.text = "Score: " + points + "/17";
-
+        PointScore.text = "Score: " + points + "/" + childs;
+            
+        Debug.Log(childs);
         if (points == 17)
         {
             YouWinMessage.text = "Find The Portal!";
