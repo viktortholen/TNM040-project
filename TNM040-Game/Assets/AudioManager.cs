ae5c3bd94763e19b2c15098a4385c1e8ea189fcf
Philip Ngo <philipngo_@hotmail.com>
2018-11-25 15:00:07 +0100

    Update AudioManager.cs

--- TNM040-Game/Assets/AudioManager.cs
+++ TNM040-Game/Assets/AudioManager.cs
@@ -11,6 +11,16 @@ public class AudioManager : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
 
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         DontDestroyOnLoad(gameObject);
 
 		foreach(Sound s in sounds)
