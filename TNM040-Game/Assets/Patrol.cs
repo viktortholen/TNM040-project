23e7cd86965b82ae517e1f41e32ad26f8f99609e
viktortholen <viktor.tholen@gmail.com>
2018-11-28 13:51:55 +0100

    Level 3 + torch

--- TNM040-Game/Assets/Patrol.cs
+++ TNM040-Game/Assets/Patrol.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Patrol : MonoBehaviour {
+public class Patrol : MonoBehaviour
+{
 
     public float speed;
     private bool movingRight = true;
     public Transform GroundDetection;
+    public GameObject Light;
 
     Vector2 direction;
 
@@ -16,30 +18,35 @@ public class Patrol : MonoBehaviour {
     }
 
 
-    void Update ()
+    void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
-       
+
         RaycastHit2D groundInfo = Physics2D.Raycast(GroundDetection.position, Vector2.down, 2f);
         RaycastHit2D wallinfo = Physics2D.Raycast(GroundDetection.position, direction, 0.1f);
 
 
-        if (groundInfo.collider == false || wallinfo.collider.CompareTag("Base") != false )
+        if (groundInfo.collider == false || wallinfo.collider.CompareTag("Base") != false)
         {
-            if(movingRight == true)
+            if (movingRight == true)
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
+                Vector3 temp = new Vector3(0, 0, 4f);
+                Light.transform.position += temp;
                 movingRight = false;
-                
+
             }
             else
             {
+                Vector3 temp = new Vector3(0, 0, 4f);
+                Light.transform.position -= temp;
                 transform.eulerAngles = new Vector3(0, 0, 0);
                 movingRight = true;
-              
+
             }
 
         }
 
     }
+
 }
