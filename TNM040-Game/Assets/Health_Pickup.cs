b1029d75ca16b687a4ac31f3a5239694ce10c302
viktortholen <viktor.tholen@gmail.com>
2018-12-05 15:31:20 +0100

    Level 3

---
+++ TNM040-Game/Assets/Health_Pickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_Pickup : MonoBehaviour {
+
+    public GameObject ps;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            GameControlScript.health += 1;
+            GameObject clone = (GameObject)Instantiate(ps, transform.position, Quaternion.identity);
+            Destroy(gameObject, 0.1f);
+            Destroy(clone, 1.0f);
+      
+        }
+
+    }
+}
